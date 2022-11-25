f8fd68f91b587ebbf4e2f14ba0fd40b8b6c4b5b5
Jan Koca <kocajan@fel.cvut.cz>
2022-11-25 12:57:33 +0100

    nothing

--- cviceni.py
+++ cviceni.py
@@ -5,7 +5,7 @@ import matplotlib
 matplotlib.use('TkAgg')
 
 # read image
-img = plt.imread('tmp/black.png')
+img = plt.imread('colors/yellow.png')
 # img = plt.imread('imgs_for_our_dear_Ales/02input_color_image.png')
 print(np.max(img))
 print(np.min(img))
@@ -16,8 +16,9 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 k = 5
-ax.scatter(img[::k, ::k, 0].flatten(), img[::k, ::k, 1].flatten(), img[::k, ::k, 2].flatten(),
-           c=img[::k, ::k, :].flatten().reshape(-1, 3), marker='o', s=5)
+
+ax.scatter(img[::k, ::k, 0].flatten(), img[::k, ::k, 1].flatten(), img[::k, ::k, 2].flatten(), c=img[::k, ::k, :]
+           .flatten().reshape(-1, 3), marker='o', s=5)
 
 ax.set_xlabel('Red')
 ax.set_ylabel('Green')
